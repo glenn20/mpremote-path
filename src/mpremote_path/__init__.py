69126a74341c4f2b8691df63bf6706b2f10b8fc4
Glenn Moloney <glenn.moloney@gmail.com>
2024-10-26 17:39:03 +1100

    pyproject.toml: Update to use `uv` for development environment.
    Use `uv` to manage development dependencies, tox configuration, and
    versioning, including adding `uv.lock` to the repository and updating
    .gitignore.
    
    Use `hatch-vcs` to manage the version number in `_version.py` based on
    git tags.

--- src/mpremote_path/__init__.py
+++ src/mpremote_path/__init__.py
@@ -1,11 +1,15 @@
 """Provides the `MPRemotePath` class which provides a `pathlib.Path` compatible
 interface to accessing and manipulating files on micropython boards via the
 `mpremote` tool.
+
+Dependencies: - `mpremote.py` for access to serial-attached ESP32 devices.
 """
+
 # Copyright (c) 2023-2024 @glenn20
 # MIT License
 
-__version__ = "0.0.1"
-
-from .board import Board  # noqa: F401
-from .mpremote_path import MPRemotePath, is_wildcard_pattern, mpremotepath  # noqa: F401
+from mpremote_path._version import __version__ as __version__
+from mpremote_path.board import Board as Board
+from mpremote_path.mpremote_path import MPRemotePath as MPRemotePath
+from mpremote_path.mpremote_path import is_wildcard_pattern as is_wildcard_pattern
+from mpremote_path.mpremote_path import mpremotepath as mpremotepath
